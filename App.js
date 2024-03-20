bef0cdd46a8135f22f29a5aae67d62bc6a77c00e
ismail öztürk <smlztrk2001@gmail.com>
2024-03-20 10:27:23 +0300

    navigation

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Login from "./screens/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import Register from "./screens/Register";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+        <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="Register" component={Register} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff8",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/Login.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Login() {
+  return (
+    <View>
+      <View style={{ backgroundColor: "red" }}>
+        <Text>deneme</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainview: {
+    flex: 1,
+    backgroundColor: "rgba(12,37,65,1)",
+  },
+});
